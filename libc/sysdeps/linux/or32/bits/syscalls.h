93588bcf760bee8b347f5ffffe648f76d8c7e292
Jonas Bonn <jonas.bonn@gmail.com>
2010-04-10 08:47:58 +0200

    Checkpoint new syscall function

--- libc/sysdeps/linux/or32/bits/syscalls.h
+++ libc/sysdeps/linux/or32/bits/syscalls.h
@@ -1,104 +1,73 @@
-#include <asm/unistd.h>
+/*
+ * Based on arm/bits/syscalls.h
+ */
 
+#ifndef _BITS_SYSCALLS_H
+#define _BITS_SYSCALLS_H
 #ifndef _SYSCALL_H
-#error "Never use <bits/syscall.h> directly; include <sys/syscall.h> instead."
+# error "Never use <bits/syscalls.h> directly; include <sys/syscall.h> instead."
 #endif
 
-extern int errno;
-void *__dso_handle;
+/*
+   Some of the sneaky macros in the code were taken from
+   glibc-2.3.2/sysdeps/unix/sysv/linux/arm/sysdep.h
+*/
 
-/* Include the __NR_<name> definitions. */
-#include <bits/sysnum.h>
+#ifdef __ASSEMBLER__
 
-#undef __syscall_return
-#define __syscall_return(type) \
-	return (__sc_ret > (unsigned long)-4096  ? errno = -__sc_ret, __sc_ret = -1 : 0), \
-	       (type) __sc_ret
+/* TODO: recheck this */
 
-#undef __syscall_clobbers
-#define __syscall_clobbers
+/* Call a given syscall, with arguments loaded.  Unlike the DO_CALL
+   macro in glibc, this macro does not load syscall arguments.  */
+#undef DO_CALL
+#define DO_CALL(syscall_name)			\
+    l.lwz r11, =SYS_ify (syscall_name);		\
+    l.sys 1					\
+    l.nop
 
-#undef _syscall0
-#define _syscall0(type,name) \
-type name(void) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-__asm__ __volatile__ ("l.sys 	1" : "=r" (__sc_ret): "r" (__sc_ret)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
+#else
 
-#undef _syscall1
-#define _syscall1(type,name,atype,a) \
-type name(atype a) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-register long __a __asm__ ("r3") = (long)(a); \
-__asm__ __volatile__ ("l.sys 	 1" \
-			: "=r" (__sc_ret) \
-			: "r" (__sc_ret), "r" (__a)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
+#include <errno.h>
 
-#undef _syscall2
-#define _syscall2(type,name,atype,a,btype,b) \
-type name(atype a,btype b) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-register long __a __asm__ ("r3") = (long)(a); \
-register long __b __asm__ ("r4") = (long)(b); \
-__asm__ __volatile__ ("l.sys 	 1" \
-			: "=r" (__sc_ret) \
-			: "r" (__sc_ret), "r" (__a), "r" (__b)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
+#define INTERNAL_SYSCALL_NCS(name, err, nr, args...)			\
+  ({ unsigned long __sys_result;					\
+     {									\
+       register long __sc_ret __asm__ ("r11") = name;			\
+       LOAD_ARGS_##nr (args)						\
+       __asm__ __volatile__ ("l.sys     1"				\
+                             : "=r" (__sc_ret)				\
+                             : "r" (__sc_ret) ASM_ARGS_##nr);		\
+       __asm__ __volatile__ ("l.nop");					\
+       __sys_result = __sc_ret;						\
+     }									\
+     (long) __sys_result; })
 
-#undef _syscall3
-#define _syscall3(type,name,atype,a,btype,b,ctype,c) \
-type name(atype a,btype b,ctype c) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-register long __a __asm__ ("r3") = (long)(a); \
-register long __b __asm__ ("r4") = (long)(b); \
-register long __c __asm__ ("r5") = (long)(c); \
-__asm__ __volatile__ ("l.sys 	 1" \
-			: "=r" (__sc_ret) \
-			: "r" (__sc_ret), "r" (__a), "r" (__b), "r" (__c)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
+#define LOAD_ARGS_0()
+#define ASM_ARGS_0
+#define LOAD_ARGS_1(a)				\
+  LOAD_ARGS_0 ()				\
+  register long __a __asm__ ("r3") = (long)(a);
+#define ASM_ARGS_1	ASM_ARGS_0, "r" (__a)
+#define LOAD_ARGS_2(a, b)			\
+  LOAD_ARGS_1 (a)				\
+  register long __b __asm__ ("r4") = (long)(b);
+#define ASM_ARGS_2	ASM_ARGS_1, "r" (__b)
+#define LOAD_ARGS_3(a, b, c)			\
+  LOAD_ARGS_2 (a, b)				\
+  register long __c __asm__ ("r5") = (long)(c);
+#define ASM_ARGS_3	ASM_ARGS_2, "r" (__c)
+#define LOAD_ARGS_4(a, b, c, d)			\
+  LOAD_ARGS_3 (a, b, c)				\
+  register long __d __asm__ ("r6") = (long)(d);
+#define ASM_ARGS_4	ASM_ARGS_3, "r" (__d)
+#define LOAD_ARGS_5(a, b, c, d, e)		\
+  LOAD_ARGS_4 (a, b, c, d)			\
+  register long __e __asm__ ("r7") = (long)(e);
+#define ASM_ARGS_5	ASM_ARGS_4, "r" (__e)
+#define LOAD_ARGS_6(a, b, c, d, e, f)		\
+  LOAD_ARGS_5 (a, b, c, d, e)			\
+  register long __f __asm__ ("r8") = (long)(f);
+#define ASM_ARGS_6	ASM_ARGS_5, "r" (__f)
 
-#undef _syscall4
-#define _syscall4(type,name,atype,a,btype,b,ctype,c,dtype,d) \
-type name (atype a, btype b, ctype c, dtype d) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-register long __a __asm__ ("r3") = (long)(a); \
-register long __b __asm__ ("r4") = (long)(b); \
-register long __c __asm__ ("r5") = (long)(c); \
-register long __d __asm__ ("r6") = (long)(d); \
-__asm__ __volatile__ ("l.sys 	 1" \
-			: "=r" (__sc_ret) \
-			: "r" (__sc_ret), "r" (__a), "r" (__b), "r" (__c), "r" (__d)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
-
-#undef _syscall5
-#define _syscall5(type,name,atype,a,btype,b,ctype,c,dtype,d,etype,e) \
-type name (atype a,btype b,ctype c,dtype d,etype e) \
-{ \
-register long __sc_ret __asm__ ("r11") = __NR_##name; \
-register long __a __asm__ ("r3") = (long)(a); \
-register long __b __asm__ ("r4") = (long)(b); \
-register long __c __asm__ ("r5") = (long)(c); \
-register long __d __asm__ ("r6") = (long)(d); \
-register long __e __asm__ ("r7") = (long)(e); \
-__asm__ __volatile__ ("l.sys 	 1" \
-			: "=r" (__sc_ret) \
-			: "r" (__sc_ret), "r" (__a), "r" (__b), "r" (__c), "r" (__d), "r" (__e)); \
-__asm__ __volatile__ ("l.nop"); \
-__syscall_return(type); \
-}
+#endif /* __ASSEMBLER__ */
+#endif /* _BITS_SYSCALLS_H */
